729e6d8fff8cb72fcd52273d0a95ce14d4e0405f
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-10-18 18:22:30 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name='color_print', # Replace with your own username
+    version='0.0.1',
+    author='Christopher Klix',
+    author_email='christopher.klix@googlemail.com',
+    description='A simple color print function for console output.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    long_description_content_type='text/markdown',
+    url='https://github.com/christopherklix/color_print',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
